2c3534262f48151b1d95b09addb33275f8abd4b9
stefolite <stefolite@gmail.com>
2026-08-14 19:27:08 +0300

    fix schemas.py import

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from pydantic import ValidationError
 from json import JSONDecodeError
 from contextlib import asynccontextmanager
-from schemas import (
+from .schemas import (
     IncomingMessage,
     MessageEvent,
     ConnectedEvent,
