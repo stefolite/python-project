6aeaae08b872ec892e30fde0cf6c98f56fcf059a
stefolite <stefolite@gmail.com>
2026-08-16 13:00:31 +0300

    create connection_manager.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from uuid import uuid4
 from pydantic import ValidationError
 from json import JSONDecodeError
 from contextlib import asynccontextmanager
-from .schemas import (
+from backend.schemas import (
     IncomingMessage,
     MessageEvent,
     ConnectedEvent,
@@ -11,6 +10,7 @@ from .schemas import (
     MemberLeftEvent,
     ErrorEvent,
 )
+from backend.connection_manager import ConnectionManager
 
 
 @asynccontextmanager
@@ -21,41 +21,6 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: dict[str, dict[str, WebSocket]] = {}
-
-    async def connect(self, websocket: WebSocket) -> str:
-        await websocket.accept()
-        connection_id = uuid4().hex[:12]
-        websocket.state.connection_id = connection_id
-        room_id = websocket.state.room_id
-        if room_id not in self.active_connections:
-            self.active_connections[room_id] = {}
-        self.active_connections[room_id][connection_id] = websocket
-        return connection_id
-
-    def disconnect(self, websocket: WebSocket):
-        room_id = websocket.state.room_id
-        connection_id = websocket.state.connection_id
-        room = self.active_connections.get(room_id)
-        if room is None:
-            return
-        room.pop(connection_id, None)
-        if not room:
-            self.active_connections.pop(room_id, None)
-
-    async def broadcast(self, room_id: str, event: dict):
-        room = self.active_connections.get(room_id)
-        if room is None:
-            return
-        for connection in list(room.values()):
-            try:
-                await connection.send_json(event)
-            except (WebSocketDisconnect, RuntimeError):
-                self.disconnect(connection)
 
 
 manager = ConnectionManager()
